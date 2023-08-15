d03138239027f3a9093af3b284bdfe31c0c83a9d
Krhystofferson Ferrari <102234632+KrhystFerrari@users.noreply.github.com>
2023-08-15 20:24:09 -0300

    feat: Aula sobre comentários com JavaScript

---
+++ 02 - Comentários em código com JavaScript/comentarios.js
@@ -0,0 +1,15 @@
+console.log("Olá, Mundo!"); // Isso é um comentário.
+
+// console.log("Olá, Mundo!");  // Isso é um trecho de código comentado (deixa de funcionar após comentado).
+
+// Comentário em bloco de código.
+/*
+console.log("Olá, Mundo!");
+console.log("Olá, Mundo!");
+console.log("Olá, Mundo!");
+console.log("Olá, Mundo!");
+console.log("Olá, Mundo!");
+console.log("Olá, Mundo!");
+console.log("Olá, Mundo!");
+console.log("Olá, Mundo!");
+*/
