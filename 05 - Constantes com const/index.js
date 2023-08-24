825bc13e768cd10340dd4ec2e901144a2c0e2ec6
Krhystofferson Ferrari <102234632+KrhystFerrari@users.noreply.github.com>
2023-08-23 21:25:18 -0300

    feat: Aula sobre constantes utilizando const

---
+++ 05 - Constantes com const/index.js
@@ -0,0 +1,20 @@
+/*
+
+- Não podemos criar constantes com palavras reservadas.
+- Constantes precisam ter nomes significativos.
+- Não se pode iniciar o nome de uma constante com um número.
+- Não podem conter espaços ou traços.
+- Utilizamos camelCase.
+- Case-sensitive.
+- Não pode ser redeclarada.
+- Não podemos mudar o valor de uma constante.
+- NÃO UTILIZE VAR E SIM CONST.
+
+*/
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero + segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+console.log(resultado, resultadoDuplicado);
