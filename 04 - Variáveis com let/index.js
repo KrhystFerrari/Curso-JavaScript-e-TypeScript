f978ac551f070ee4b611297f26c2c38564acd650
Krhystofferson Ferrari <102234632+KrhystFerrari@users.noreply.github.com>
2023-08-23 21:03:39 -0300

    feat: Aula sobre variáveis utilizando let

---
+++ 04 - Variáveis com let/index.js
@@ -0,0 +1,17 @@
+/*
+
+- Não podemos criar variáveis com palavras reservadas.
+- Variáveis precisam ter nomes significativos.
+- Não se pode iniciar o nome de uma variável com um número.
+- Não podem conter espaços ou traços.
+- Utilizamos camelCase.
+- Case-sensitive.
+- Não podemos redeclarar variáveis com let.
+- NÃO UTILIZE VAR E SIM LET.
+
+*/
+
+let nomeCliente = "Jõao";
+nomeCliente = "Silva";
+
+console.log(nomeCliente);
